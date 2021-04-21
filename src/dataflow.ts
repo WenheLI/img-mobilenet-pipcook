504f469485f982e40f9e042c976dbe7bb5d046d6
eric.lwh <eric.lwh@alibaba-inc.com>
2021-04-21 17:44:27 +0800

    add

---
+++ src/dataflow.ts
@@ -0,0 +1,45 @@
+import { DataFlowEntry, ScriptContext } from '@pipcook/core';
+import { Dataset } from '@pipcook/datacook';
+import { transformDataset } from '@pipcook/datacook/dist/dataset';
+
+const resizeEntry: DataFlowEntry<Dataset.Types.Sample, Dataset.Types.ImageDatasetMeta> = async (dataset: Dataset.Types.Dataset<Dataset.Types.Sample, any>, options: Record<string, any>, context: ScriptContext)  => {
+  const [x='-1', y='-1'] = options['size'];
+  const parsedX = parseInt(x);
+  const parsedY = parseInt(y);
+  if (parsedX == -1 || parsedY == -1) return;
+
+  return transformDataset<Dataset.Types.ImageDatasetMeta, Dataset.Types.Sample>({
+    next: async (sample) => {
+      const resized = sample.data.resize(parsedX, parsedY);
+      const { normalize = false } = options;
+      if (normalize) return {
+        data: context.dataCook.Image.normalize(resized.toTensor()),
+        label: sample.label,
+      }
+      return {
+        data: resized.toTensor(),
+        label: sample.label
+      }
+    },
+    metadata: async (meta) => {
+      return {
+        ...meta,
+        dimension: {
+          x: parsedX,
+          y: parsedY,
+          z: meta.dimension.z
+        }
+      }
+    }
+  }, dataset)
+}
+
+/**
+ * this is the data process plugin to process pasvoc format data. It supports resize the image and normalize the image
+ * @param resize =[256, 256][optional] resize all images to same size
+ * @param normalize =false[optional] if normalize all images to have values between [0, 1]
+ */
+export default resizeEntry;
+
+
+
